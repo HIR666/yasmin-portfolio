32e1f0f2e5ba136f9077f7522ea78314abe4f6d5
HIR666 <stinker.muthafuker@gmail.com>
2024-01-18 15:54:19 +0300

    switched to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import "./App.css";
 import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/Home";
 
 const theme = createTheme({
@@ -18,12 +18,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/profile" element={<HomeScreen />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </ThemeProvider>
   );
 }
